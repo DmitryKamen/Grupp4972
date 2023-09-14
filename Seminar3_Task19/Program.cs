5ef03746938941e5bcbb9ead3cc9e0ec006c9030
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-09-14 15:10:59 +0200

    Домашняя работа № 3 выполнена

---
+++ Seminar3_Task19/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Answer
+{
+
+    static bool IsPalindrome(int number)
+    {
+        if (number > 99999 || number < 10000)
+        {
+            Console.WriteLine(number + " - Число не пяти значное");
+            return false;
+        }
+        else
+        {
+            string num = Convert.ToString(number);
+            if (num[0] == num[4] && num[1] == num[3])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+    static public void Main(string[] args)
+    {
+        int number;
+
+        if (args.Length >= 1)
+        {
+            number = int.Parse(args[0]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 234322;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
