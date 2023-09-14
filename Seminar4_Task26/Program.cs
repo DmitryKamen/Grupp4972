5c9d55b3e88df65a6f638ea1e4f3fe58499c9a7b
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-09-15 00:07:14 +0200

    Seminar4 Task 24,26,28,30

---
+++ Seminar4_Task26/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача №26
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// int ReadData(string msg) // вводим данные
+// {
+//     Console.WriteLine(msg);
+//     int num = int.Parse(Console.ReadLine() ?? "0");
+//     return num;
+// }
+
+// int numLength(int n)
+// {
+//     return Convert.ToInt32(Math.Log(n,10)+1);
+// }
+// int n = ReadData("Введите число");
+// Console.WriteLine("количество цифр в числе = " + numLength(n));
+
+
+// Вывод сообщения и запись введённых данных
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+
+// Считает длинну числа
+int NumberLength(int number)
+{
+    int length = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        length++;
+    }
+    return length;
+}
+
+int number = Prompt("Введите число: ");
+Console.WriteLine(NumberLength(number));
