7bfdc730f04f08a72ae0ebd777f643d2ab31b3d9
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-09-15 17:20:32 +0200

    Домашняя работа № 4 выполнена

---
+++ Seminar4_Task25/Program.cs
@@ -0,0 +1,37 @@
+﻿// №25 Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+//Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+long Degree (int A, int B)
+{
+    long res = 1;
+    while ( B > 0 )
+    {
+        res = res * A;
+        B = B - 1;
+    }
+    return res;
+}
+// Вводим числа
+int numberA = ReadData("Введите число A: ");
+int numberB = ReadData("Введите число B: ");
+long res = Degree(numberA, numberB); // Выводим A в степень B
+PrintResult("Число - "+ numberA + " В степени - "+ numberB + " Является число - " + res); // Печатаем результат
---
+++ Seminar4_Task27/Program.cs
@@ -0,0 +1,59 @@
+﻿// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Принимает на вход число и выдаёт сумму цифр в числе
+
+int SunDigitWhile(int num)
+{
+    int res = 0;
+    while(num > 0)
+    {
+        res = res + num % 10;
+        num = num / 10;
+    }
+    return res;
+}
+// Вводим число
+int number = ReadData("Введите число: ");
+
+DateTime d1 = DateTime.Now;
+int result1 = SunDigitWhile(number); // Выдаёт сумму цифр в числе
+Console.WriteLine(DateTime.Now-d1);
+
+DateTime d2 = DateTime.Now;
+int result2 = SunDigitToString(number);
+Console.WriteLine(DateTime.Now-d2);
+
+PrintResult("Сумма цифр в числе - " + number + " = " + result1); // Печатаем результат
+PrintResult("Сумма цифр в числе - " + number + " = " + result2); // Печатаем результат
+
+// Через стринг - Принимает на вход число и выдаёт сумму цифр в числе
+int SunDigitToString(int num)
+{
+    int res = 0;
+    string numberOfString = Convert.ToString(num);
+    for(int i = 0; i < numberOfString.Length; i++)
+    {
+        res += Convert.ToInt32(Convert.ToString(numberOfString[i]));
+    } 
+    return res;
+}
+
+// вывод - метод SunDigitToString быстрее метода - SunDigitWhile
+
