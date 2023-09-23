a5ea46e8780bb8d0836497605f583350636650ac
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-09-23 19:20:46 +0200

    Домашняя работа № 6 выполнена

---
+++ Seminar6_Task41/Program.cs
@@ -0,0 +1,54 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int NumberOfInputs(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+
+int[] ArrayAssembly( int number)
+{
+    int count = number;
+    int [] arr = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine()??"0");
+    }
+    return arr;
+}
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+        
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int NumbersGreaterThanZero( int[]arr )
+{
+    int countAboveZero = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            countAboveZero++;
+        }
+    }
+    return countAboveZero;
+} 
+
+int arraySize = NumberOfInputs("Введите размер массива: ");
+int [] array = ArrayAssembly(arraySize);
+PrintArray(array);
+Console.WriteLine( "Колличество чисел больше нуля = "+ NumbersGreaterThanZero(array));
---
+++ Seminar6_Task43/Program.cs
@@ -0,0 +1,32 @@
+﻿// №43 Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+double[] IntersectionPoint (int b1, int b2, int k1, int k2 )
+{
+    double[] xy = new double[2];
+    xy[0] = (double) - (b1 - b2)/(k1 - k2);
+    xy[1] = k1 * xy[0] + b1;
+    return xy;
+}
+
+// void PrintResult(string msg, double x, double y)
+// {
+//     Console.WriteLine(msg, x, y);
+// }
+
+int b1 = ReadData("Введите b1: ");
+int b2 = ReadData("Введите b2: ");
+int k1 = ReadData("Введите k1: ");
+int k2 = ReadData("Введите k2: ");
+double[]xy = IntersectionPoint(b1,b2,k1,k2);
+double x = xy[0];
+double y = xy[1];
+Console.WriteLine(" Точка пересеченияя равна - " + "(" + x + ";"+ y + ")");
