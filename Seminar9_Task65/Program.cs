5fe9afade1e0ef175d43616577c1e69476d40bd0
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-09-30 21:44:57 +0200

    Seminar8 Task 61,62 and Seminar9 Task 63,65,67,69

---
+++ Seminar9_Task65/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача № 65 Задайте значения от m до n. Напишите программу которая найдет и выведет 
+// все натуральные числа от m до n.
+
+// Вывод сообщения и запись введённых данных
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+
+// Формирование строки с числами
+string SearchForNaturalElementsRec (int m, int n)
+{
+    return n >= m ? $"{m} " + SearchForNaturalElementsRec(m + 1, n) : "";
+}
+
+int m = Prompt("Введите m: ");
+int n = Prompt("Введите n: ");
+
+Console.WriteLine((m>n)?SearchForNaturalElementsRec(n, m):SearchForNaturalElementsRec(m, n));
